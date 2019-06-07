e66bb6288d9d59119361d4a7474013219575a886
Cesar Acual <chechaacual@gmail.com>
2019-06-06 21:55:25 -0600

    Verificar restriccion de acceso y redireccion con TDD

--- routes/public.php
+++ routes/public.php
@@ -19,13 +19,3 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-// Posts
-Route::get('posts/create', [
-    'uses' => 'CreatePostController@create',
-    'as' => 'posts.create'
-]);
-
-Route::post('posts/create', [
-    'uses' => 'CreatePostController@store',
-    'as' => 'posts.store'
-]);
