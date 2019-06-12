b68e8acfc0bf19fe7c56fe8df161283c61279d6c
Cesar Acual <chechaacual@gmail.com>
2019-06-11 22:56:25 -0600

    Mostrar el detalle de un registro con TDD

--- routes/public.php
+++ routes/public.php
@@ -19,3 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('posts/{post}', [
+    'as' => 'posts.show',
+    'uses' => 'PostController@show'
+])->where('post', '\d+');
+
