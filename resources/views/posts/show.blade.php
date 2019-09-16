a6876f26d7e4124af9e91114acd2d6a1b2f8f504
Cesar Acual <chechaacual@gmail.com>
2019-09-16 17:10:26 -0600

    Estilos para la vista de detalle de un post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,47 +1,71 @@
 @extends('layouts/app')
 
 @section('content')
-    <h1>{{ $post->title }}</h1>
+    <div class="row">
+        <div class="col-md-10">
+            <h1>{{ $post->title }}</h1>
+        </div>
+    </div>
 
-    {!! $post->safe_html_content !!}
+    <div class="row">
+        <div class="col-md-8">
+            <p>
+                Publicado por <a href="#">{{ $post->user->name }}</a>
+                {{ $post->created_at->diffForHumans() }}
+                en <a href="{{ $post->category->url }}">{{ $post->category->name }}</a>
+                @if ($post->pending)
+                    <span class="label label-warning">Pendiente</span>
+                @else
+                    <span class="label label-success">Completado</span>
+                @endif
+            </p>
 
-    <p>{{ $post->user->name }}</p>
+            {!! $post->safe_html_content !!}
 
-    @if(auth()->check())
-        @if (!auth()->user()->isSubscribedTo($post))
-            {!! Form::open(['route' => ['posts.subscribe', $post], 'method' => 'POST']) !!}
-                <button type="submit">Suscribirse al post</button>
-            {!! Form::close() !!}
-        @else
-            {!! Form::open(['route' => ['posts.unsubscribe', $post], 'method' => 'DELETE']) !!}
-                <button type="submit">Desuscribirse del post</button>
-            {!! Form::close() !!}
-        @endif
-    @endif
-
-    <h4>Comentarios</h4>
-
-    {!! Form::open(['route' => ['comments.store', $post], 'method' => 'POST']) !!}
-
-        {!! Field::textarea('comment') !!}
-
-        <button type="submit">Publicar comentario</button>
-
-    {!! Form::close() !!}
-
-    @foreach ($post->latestComments() as $comment)
-        <article class="{{ $comment->answer ? 'answer' : '' }}">
-
-            {!! $comment->safe_html_comment !!} - {{ $comment->post->user->name }}
-
-            @if(Gate::allows('accept', $comment) && !$comment->answer)
-            {!! Form::open(['route' => ['comments.accept', $comment], 'method' => 'POST']) !!}
-                <button type="submit">Aceptar respuesta</button>
-            {!! Form::close() !!}
+            @if(auth()->check())
+                @if (!auth()->user()->isSubscribedTo($post))
+                    {!! Form::open(['route' => ['posts.subscribe', $post], 'method' => 'POST']) !!}
+                    <button type="submit">Suscribirse al post</button>
+                    {!! Form::close() !!}
+                @else
+                    {!! Form::open(['route' => ['posts.unsubscribe', $post], 'method' => 'DELETE']) !!}
+                    <button type="submit">Desuscribirse del post</button>
+                    {!! Form::close() !!}
+                @endif
             @endif
-        </article>
-    @endforeach
 
-    {{ $post->latestComments()->render() }}
+            <hr>
+
+            <h4>Comentarios</h4>
+
+            @foreach ($post->latestComments() as $comment)
+                <article class="{{ $comment->answer ? 'answer' : '' }}">
+
+                    {!! $comment->safe_html_comment !!} - {{ $comment->post->user->name }}
+
+                    @if(Gate::allows('accept', $comment) && !$comment->answer)
+                        {!! Form::open(['route' => ['comments.accept', $comment], 'method' => 'POST']) !!}
+                        <button type="submit">Aceptar respuesta</button>
+                        {!! Form::close() !!}
+                    @endif
+                </article>
+                <hr>
+            @endforeach
+
+            {{ $post->latestComments()->render() }}
+
+            {!! Form::open(['route' => ['comments.store', $post], 'method' => 'POST', 'class' => 'form']) !!}
+
+                {!! Field::textarea('comment', ['class' => 'form-control', 'rows' => 6, 'label' => 'Escribe un comentario']) !!}
+
+                <button type="submit" class="btn btn-primary">Publicar comentario</button>
+
+            {!! Form::close() !!}
+        </div>
+
+        <div class="col-md-2">
+
+        </div>
+    </div>
 
 @endsection
